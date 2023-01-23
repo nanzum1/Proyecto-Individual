73566940b00b59b96ac3880c6c4b9f0ce6bf4f29
nanzum1 <adatos.fer@gmail.com>
2023-01-23 13:54:09 -0300

    con reloj

--- js/main.js
+++ js/main.js
@@ -244,3 +244,27 @@ function mostrarCalendario(year,month)
 }
  
 mostrarCalendario(actual.getFullYear(),actual.getMonth()+1);
+
+
+
+
+function muestraReloj() {
+   
+    var fechaHora = new Date();
+    var horas = fechaHora.getHours();
+    var minutos = fechaHora.getMinutes();
+    var segundos = fechaHora.getSeconds();
+    var day = (fechaHora.getDate());
+ 
+
+    if(horas < 10) { horas = '0' + horas; }
+    if(minutos < 10) { minutos = '0' + minutos; }
+    if(segundos < 10) { segundos = '0' + segundos; }
+
+
+    document.getElementById("reloj").innerHTML = horas+':'+minutos+':'+segundos;
+  }
+
+    window.onload = function() {
+    setInterval(muestraReloj, 1000);
+  }
